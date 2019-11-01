ee5c925eabfc931845c7e7aa56c3311faaaa79ca
nix.long <nixlong@126.com>
2019-11-01 15:59:27 +0800

    fix py file bug: replace blank character with tab

--- ports/gr55xx/examples/xblepy_user_delegate.py
+++ ports/gr55xx/examples/xblepy_user_delegate.py
@@ -8,15 +8,15 @@
 import xblepy,ble
 class MyGapHandler(xblepy.DefaultGapDelegate):
     def __init__(self):
-        pass    
+        pass
     def transefr_hex(bytes):
-        l = [hex(int(i)) for i in bytes]
-        return (" ".join(l))
+        l = [hex(int(i)) for i in bytes]
+        return (" ".join(l))
     def handleAdvStartEvent(self, status):
         print('+++ adv start: called from MyGapHandler %d ' % status)
     def handleAdvStopEvent(self, status):
         print('+++ adv stop: called from MyGapHandler %d ' % status)
-    def handleConnectEvent(self, status, peer_addr):        
+    def handleConnectEvent(self, status, peer_addr):
         l = [hex(int(i)) for i in peer_addr]
         l.reverse()
         addr = ":".join(l)
@@ -24,8 +24,30 @@ class MyGapHandler(xblepy.DefaultGapDelegate):
     def handleDisconnectEvent(self, status):
         print('+++ gap disconnect: called from MyGapHandler %d ' % status)
 
-ble.enable()
-periph = xblepy.Peripheral()
-handler = MyGapHandler()
-periph.setGapDelegate(handler)
-periph.startAdvertise(device_name="mpy_test")
+class MyGattsHandler(xblepy.DefaultGattsDelegate):
+    def handleReadEvent(self, idx):
+        print('+++ MyGattsHandler:read idx %d' % idx)
+        MyGattsHandler.responseRead(idx, 'hello')
+    def handleWriteEvent(self, idx, offset, data):
+        print('+++ MyGattsHandler:write idx %d ,offset %d' % (idx, offset))
+        MyGattsHandler.responseWrite(idx, True)
+
+if __name__ == '__main__':
+    ble.enable()
+    s1 = xblepy.Service(1, xblepy.UUID("6e400001-b5a3-f393-e0a9-e50e24dcca98"))
+    c1 = xblepy.Characteristic(2, xblepy.UUID("6e400002-b5a3-f393-e0a9-e50e24dcca98"))
+    c2 = xblepy.Characteristic(3, xblepy.UUID("6e400004-b5a3-f393-e0a9-e50e24dcca98"))
+    d1 = xblepy.Descriptor(4, xblepy.UUID("6e400003-b5a3-f393-e0a9-e50e24dcca98"))
+    d2 = xblepy.Descriptor(5, xblepy.UUID(0x2902))
+    d3 = xblepy.Descriptor(6, xblepy.UUID(0x2902))
+    c1.addDescriptor(d1)
+    c1.addDescriptor(d2)
+    c2.addDescriptor(d3)
+    s1.addCharacteristic(c1)
+    s1.addCharacteristic(c2)
+    periph = xblepy.Peripheral()
+    periph.setGapDelegate(MyGapHandler())
+    periph.setGattsDelegate(MyGattsHandler())
+    periph.addService(s1)
+    periph.startServices()
+    periph.startAdvertise(device_name="mpy_test")
